82a964fd29ce3db4d8b2ac05080cb19f4c8aa076
Yury <bezlla@yandex.ru>
2023-03-14 17:29:18 +0300

    Added base configuration shop service

---
+++ src/Shop/Shop.Core/ShopConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shop.Core;
+
+public static class ShopConfiguration
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        return serviceCollection;
+    }
+}
