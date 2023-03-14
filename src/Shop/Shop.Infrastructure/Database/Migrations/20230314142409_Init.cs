7c7a9a1edd024d6e7ecb319c256135e8741199d0
Yury <bezlla@yandex.ru>
2023-03-14 17:24:23 +0300

    Added migrations for Shop service

---
+++ src/Shop/Shop.Infrastructure/Database/Migrations/20230314142409_Init.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductCount = table.Column<int>(type: "integer", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderEntities", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderEntities_ProductId",
+                table: "OrderEntities",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderEntities_UserId",
+                table: "OrderEntities",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderEntities");
+        }
+    }
+}
