d8038c2f332d4251c7144871ee1804f9656e1a3e
Yury <bezlla@yandex.ru>
2023-03-14 16:43:00 +0300

    Implemented stock provider

---
+++ src/Stock/Interfaces.Stock/Interfaces/IStockProvider.cs
@@ -0,0 +1,9 @@
+﻿using Interfaces.Stock.Requests;
+
+namespace Interfaces.Stock.Interfaces;
+
+public interface IStockProvider
+{
+    Task<StockGetProductListResponse> GetProductList(StockGetProductListRequest request, TimeSpan? timeOut = null);
+    Task<StockSupplyProductResponse> SupplyProduct(StockSupplyProductRequest request, TimeSpan? timeOut = null);
+}
---
+++ src/Stock/Interfaces.Stock/StockProvider.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces.Stock.Interfaces;
+using Interfaces.Stock.Requests;
+using MassTransit;
+
+namespace Interfaces.Stock;
+
+public class StockProvider : IStockProvider
+{
+    private readonly IRequestClient<StockGetProductListRequest> _getProductListClient;
+    private readonly IRequestClient<StockSupplyProductRequest> _supplyProductClient;
+
+    public StockProvider(IRequestClient<StockGetProductListRequest> getProductListClient, 
+        IRequestClient<StockSupplyProductRequest> supplyProductClient)
+    {
+        _getProductListClient = getProductListClient;
+        _supplyProductClient = supplyProductClient;
+    }
+
+    public async Task<StockGetProductListResponse> GetProductList(StockGetProductListRequest request, TimeSpan? timeOut = null)
+    {
+        var response = await _getProductListClient.GetResponse<StockGetProductListResponse>(request, 
+            timeout: timeOut ?? RequestTimeout.Default);
+        return response.Message;
+    }
+
+    public async Task<StockSupplyProductResponse> SupplyProduct(StockSupplyProductRequest request, TimeSpan? timeOut = null)
+    {
+        var response = await _supplyProductClient.GetResponse<StockSupplyProductResponse>(request, 
+            timeout: timeOut ?? RequestTimeout.Default);
+        return response.Message;
+    }
+}
