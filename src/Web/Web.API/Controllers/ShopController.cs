0d1cead0565bd218d62165adbbba9f966da0f03d
Yury <bezlla@yandex.ru>
2023-03-29 14:49:24 +0300

    Implemented base web configuration

---
+++ src/Web/Web.API/Controllers/ShopController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class ShopController : ControllerBase
+{
+    
+}
--- src/Web/Web.API/Program.cs
+++ src/Web/Web.API/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.OpenApi.Models;
+using Web.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "Web.API", Version = "v1"});
+});            
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
