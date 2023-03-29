1d11fe414b25dccbf348e22c9bf86b7479f73423
Yury <bezlla@yandex.ru>
2023-03-29 14:43:21 +0300

    Configured masstransit for Web service

---
+++ src/Web/Web.Infrastructure/WebConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Interfaces.Shop;
+using Interfaces.Shop.Interfaces;
+using Interfaces.Stock;
+using Interfaces.Stock.Interfaces;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Web.Infrastructure;
+
+public static class WebConfiguration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection.AddScoped<IStockProvider, StockProvider>();
+        serviceCollection.AddScoped<IShopProvider, ShopProvider>();
+        
+        serviceCollection.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(configuration["RABBIT_HOST"], "/", h =>
+                {
+                    h.Username(configuration["RABBIT_LOGIN"]);
+                    h.Password(configuration["RABBIT_PASSWORD"]);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+
+        return serviceCollection;
+    }
+}
